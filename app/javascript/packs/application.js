9177fd1436db994a59d9654c541e3fa5947fcf18
cabanga <manucabanga@gmail.com>
2022-07-17 08:37:12 +0100

    this commit config notifications

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,8 +2,11 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
-require("./animate")
+//require("./animate")
 require("jquery")
+
+global.toastr = require("toastr")
+
 
 document.addEventListener("turbolinks:load", function(event) {
 
